e21e2bc09b554af6bdf5a98d0fdd9fc7a2d7926b
Joseph Kuria <gakoijoseph@outlook.com>
2022-09-08 15:45:21 -0700

    Task 3

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *Description of main - prints the string "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
