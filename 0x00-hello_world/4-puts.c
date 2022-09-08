f68ee731d0023284048e6520d734415de7533993
Joseph Kuria <gakoijoseph@outlook.com>
2022-09-08 16:00:17 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *Description of main - prints the string "Programming is like building a multilingual puzzle
+ * main - prints the string "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
