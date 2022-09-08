976d9dd603f3e86f7c6f7e5f0bba67de0f142c79
Joseph Kuria <gakoijoseph@outlook.com>
2022-09-08 16:48:24 -0700

    gtndj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
+#include <stdio.h>                                                                       
 
+                                                                                         
 
-
-/**
+/**                                                                                      
  *
- *  main - "Entry point
+ *  *  main - "point start                                                                  
  *
- *    * Return: Always 1 (Success)
+ *   *                                                                                       
  *
- */
+ *    * Return: Always 0 (No fail)                                                            
+ *
+ *     */                                                                                      
 
 int main(void)
 
 {
-	write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",61);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
 
 	    return (1);
 
