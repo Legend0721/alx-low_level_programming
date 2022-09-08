1f1248b5ceaebb32273006c64da6aa3cbd7a6ed3
Joseph Kuria <gakoijoseph@outlook.com>
2022-09-08 16:07:31 -0700

    New Task log

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+nclude <stdio.h>
+
+/**
+ * main - prints the string plus new line
+ * Return: 0 if exit is true, non-zero otherwise
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
