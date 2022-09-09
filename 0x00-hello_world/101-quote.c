950dd30bd64162dce3eb1f06c3551ab007bf0e72
Joseph Kuria <gakoijoseph@outlook.com>
2022-09-08 17:01:11 -0700

    b

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,16 +3,10 @@
 
 
 /**
- *
- *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *
- *   * followed by a new line, to standard error.
- *
- *    *
- *
- *     * Return: Always 1.
- *
- *      */
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
+ * Return: Always 1.
+ */
 
 int main(void)
 
