5233fe69cf422cc141b225f9bb8cd64d443cc449
Joseph Kuria <gakoijoseph@outlook.com>
2022-09-09 17:04:21 -0700

    Advanced lvl

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - "prints all possible different combinations of two digits.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int x,y;
+
+		for (x = 0; x <= 9; x++)
+		{
+			for (y = x + 1; y <= 9; y++)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				if (x < 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		putchar('\n');
+
+	return (0);
+}
