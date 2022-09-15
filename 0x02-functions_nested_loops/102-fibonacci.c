590e5ce6a657be35650a1ccfbf9854f521c58d34
Joseph Kuria <gakoijoseph@outlook.com>
2022-09-14 17:01:56 -0700

    Advance 3

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,13 +13,13 @@ int main(void)
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (count = 0; count >= 0; count++)
+	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu", sum);
 		fib1 = fib2;
 		fib2 = sum;
-		if (sum > 3999999)
+		if (count == 49)
 		printf("\n");
 		else
 		printf(", ");
