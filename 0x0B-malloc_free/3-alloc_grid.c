111935f5eccce0b7531fdb2a5e2be3cbfe1714db
Joseph Kuria <gakoijoseph@outlook.com>
2022-10-05 15:19:31 -0700

    refresh

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	array = (int **) malloc(sizeof(int *) * height);
 
-if (array != NULL)
+	if (array != NULL)
 	{
 		for (; i < height; i++)
 	{
