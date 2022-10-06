5c43f8cd4786d411be50201b5ce0d91f26fd7ccf
Joseph Kuria <gakoijoseph@outlook.com>
2022-10-06 09:35:25 -0700

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,17 +1,15 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-/**
- * malloc_checked - Allocates memory using malloc.
- * @b: The number of bytes to be allocated.
-* Return: A pointer to the allocated memory.
- */
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmedb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-void *malloc_checked(unsigned int b)
-{
-	void *mem = malloc(b);
-
-	if (mem == NULL)
-		exit(98);
-	return (mem);
-}
+#endif
