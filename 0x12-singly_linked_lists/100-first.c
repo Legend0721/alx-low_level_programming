9b0ff2b8be263e912775d65a752588edf2423cd6
Joseph Kuria <gakoijoseph@outlook.com>
2022-10-24 15:08:31 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -14,8 +14,3 @@ void  __attribute__((constructor)) before_main()
 	printf("You\'re beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
-101-hello_holberton.asm
-
-%define sys_write 1
-%define stdout 1
